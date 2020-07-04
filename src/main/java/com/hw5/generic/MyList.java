ffd2d52df47095d9740a305f57dd186cade63e0a
Анна <haborysova@gmail.com>
2020-07-04 23:11:41 +0300

    Fix: add interface instead of realization `private ArrayList<T> list`

--- src/main/java/com/hw5/generic/MyList.java
+++ src/main/java/com/hw5/generic/MyList.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class MyList<T extends Number> {
-    private ArrayList<T> list;
+    List list = new ArrayList<T>();
 
     public MyList() {
         list = new ArrayList<T>();
@@ -15,12 +15,12 @@ public class MyList<T extends Number> {
         list.add(data);
     }
 
-    public T findMinValue() {
+    public Object findMinValue() {
         Collections.sort((List) list);
         return list.get(0);
     }
 
-    public T findMaxValue() {
+    public Object findMaxValue() {
         Collections.sort(list, Collections.reverseOrder());
         return list.get(0);
     }
